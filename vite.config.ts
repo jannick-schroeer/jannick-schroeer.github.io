b5218a780921b5bef60f403dc2d28f6b3eef97c8
Jannick Schröer <jannick.schroer@gmail.com>
2025-10-20 10:59:36 +0200

    Updated Vite Build settings to hopefully fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const build_date = new Date().toISOString().replace(/[^0-9]/g, '')
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,4 +17,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+    build: {
+        sourcemap: false,
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name]-[hash]-${build_date}.js`,
+                chunkFileNames: `assets/[name]-[hash]-${build_date}.js`,
+                assetFileNames: `assets/[name]-[hash]-${build_date}.[ext]`,
+                /*manualChunks(id) {
+                  const HugeLibraries = ['jspdf', 'libphonenumber-js', 'v-calendar']; // modify as required based on libraries in use
+                  if (HugeLibraries.some((libName) => id.includes(`node_modules/${libName}`))) {
+                    return id.toString().split("node_modules/")[1].split("/")[0].toString();
+                  }
+                }*/
+            }
+        }
+    }
 })
